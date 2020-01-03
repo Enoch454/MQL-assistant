aa6e70f47805e66c0d11c4fdf6f01bacb1d13e4c
enoch454 <aegc.45@gmail.com>
2020-01-02 21:04:23 -0600

    ajustes de segunda reunion

--- mql.sql
+++ mql.sql
@@ -1,30 +1,47 @@
 CREATE DATABASE mql
 
 -------------------------------------------------------------------------------
+-- TABLE: DetalleEmpresa
+-------------------------------------------------------------------------------
+drop table if exists DetalleEmpresa
+create table DetalleEmpresa(
+    id_DetalleEmpresa int not null,
+    esBinario BINARY not null,
+    inicioSemana date not null,
+    finSemana date not null,
+    PRIMARY KEY (id_DetalleEmpresa)
+
+)
+grant select, insert, update, delete on DetalleEmpresa to public
+
+-------------------------------------------------------------------------------
 -- TABLE: Empresa
 -------------------------------------------------------------------------------
 drop table if exists Empresa
 create table Empresa(
-    id_empresa INT,
-    nombre VARCHAR(50),
+    id_empresa int not null,
+    nombre VARCHAR(50) not null,
     telefono VARCHAR(10),
     direcccion VARCHAR(100),
     PRIMARY KEY id_empresa,
-    CONSTRAINT emp_anal FOREIGN KEY Analista REFERENCES(id_analista),
+    CONSTRAINT FK_detalle FOREIGN KEY id_detalle REFERENCES DetalleEmpresa(id_DetalleEmpresa),
+
 )
 
 grant select, insert, update, delete on Empresa to root
-
 
 -------------------------------------------------------------------------------
 -- TABLE: Analista
 -------------------------------------------------------------------------------
 drop table if exists Analista
 create table Analista(
-  id_analista INT,
-  nombre VARCHAR(50),
-  id_estilo INT,
+  id_analista int not null,
+  nombre VARCHAR(50) not null,
+  id_estilo int not null,
+  id_empresa int not null,
   PRIMARY KEY id_analista,
+  CONSTRAINT FK_estilo FOREIGN KEY id_estilo REFERENCES Estilo(id_estilo),
+  CONSTRAINT FK_empresa FOREIGN KEY id_empresa REFERENCES Empresa(id_empresa)
 )
 
 grant select, insert, update, delete on Analista to public
@@ -35,172 +52,117 @@ grant select, insert, update, delete on Analista to public
 -------------------------------------------------------------------------------
 drop table if exists Corte
 create table Corte(
-    id_corte INT,
-    id_estilo INT,
-    id_cliente INT,
+    id_corte int not null,
+    id_estilo int not null,
+    id_cliente int not null,
+    cantidadPiezas int not null,
     PRIMARY KEY id_corte,
+    CONSTRAINT FK_estilo FOREIGN KEY id_estilo REFERENCES Estilo(id_estilo),
+    CONSTRAINT FK_cliente FOREIGN KEY id_cliente REFERENCES Cliente(id_cliente)
+
 )
 
 grant select, insert, update, delete on Corte to public
 
-  -------------------------------------------------------------------------------
-  -- TABLE: Cliente
-  -------------------------------------------------------------------------------
-  drop table if exists Cliente
-  go
-
-
-  create table Cliente(
-    id_cliente INT,
-    nombre VARCHAR(50),
-    telefono VARCHAR(10),
-    direcccion VARCHAR(100),
-    PRIMARY KEY id_cliente,
-  )
-  go
-
-
-  grant select, insert, update, delete on TABLE_NAME to public
-  go
-
-  -------------------------------------------------------------------------------
-  -- TABLE: Estilo
-    -------------------------------------------------------------------------------
-
-  drop table if exists Estilo
-    go
-
-
-  create table Estilo
-    id_estilo int,
-    nombre VARCHAR(50),
+-------------------------------------------------------------------------------
+-- TABLE: Estilo
+-------------------------------------------------------------------------------
+drop table if exists Estilo
+create table Estilo(
+    id_estilo int not null,
+    nombre VARCHAR(50) not null,
     comentarios TEXT,
     PRIMARY KEY id_estilo,
-  )
+)
 
+grant select, insert, update, delete on Estilo to public
 
-  grant select, insert, update, delete on Estilo
-    to public
+-------------------------------------------------------------------------------
+-- TABLE: Jornada
+-------------------------------------------------------------------------------
+drop table if exists Jornada
+create table Jornada(
+    id_jornada int not null,
+    id_estilo int not null,
+    esBihorario boolean not null,
+    PRIMARY KEY id_jornada,
+    CONSTRAINT FK_estilo FOREIGN KEY (id_estilo) REFERENCES Estilo(id_estilo)
+)
+grant select, insert, update, delete on Jornada to public
 
-  -------------------------------------------------------------------------------
-  -- TABLE: Jornada
-  -------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- TABLE: Operacion
+-------------------------------------------------------------------------------
+drop table if exists Operacion
+create table Operacion(
+      id_operacion int not null,
+      id_jornada int not null,
+      tareaHora int not null,
+      totalProducido int not null,
+      PRIMARY KEY (id_operacion),
+      CONSTRAINT FK_jornada FOREIGN KEY (id_jornada) REFERENCES Jornada(id_jornada)
+)
+grant select, insert, update, delete on Operacion to public
 
-  drop table if exists Jornada
+-------------------------------------------------------------------------------
+-- TABLE: Hora
+-------------------------------------------------------------------------------
+drop table if exists Hora
+create table Hora(
+    id_jornada int not null,
+    esExtra boolean not null,
+    horaRevicion TIME not null,
+    PRIMARY KEY horaRevicion
+)
+grant select, insert, update, delete on Hora to public
 
+-------------------------------------------------------------------------------
+-- TABLE: OperacionOperario
+-------------------------------------------------------------------------------
+drop table if exists OperacionOperario
+create table OperacionOperario(
+    id_operacionOperario int not null,
+    id_operacion int not null,
+    id_operario int not null,
+    id_hora int not null,
+    tareaHora int not null,
+    PRIMARY KEY id_operacionOperario,
+    CONSTRAINT FK_operacion FOREIGN KEY (id_operacion) REFERENCES Operacion(id_operacion),
+    CONSTRAINT FK_operario FOREIGN KEY (id_operario) REFERENCES Operario(id_operario),
+    CONSTRAINT FK_hora FOREIGN KEY (id_hora) REFERENCES Operario(id_hora)
 
-  create table Jornada(
-      id_jornada int,
-      id_estilo INT,
-      esBihorario BOOLEAN,
+)
+grant select, insert, update, delete on OperacionOperario to public
 
-  )
-  go
+-------------------------------------------------------------------------------
+-- TABLE: Operario
+-------------------------------------------------------------------------------
+drop table if exists Operario
+create table Operario(
+    id_operario int not null,
+    id_sueldo int not null,
+    id_empresa int not null,
+    nombre VARCHAR(50) not null,
+    direcccion VARCHAR(100),
+    telefono VARCHAR(10),
+    PRIMARY KEY (id_operario),
+    CONSTRAINT FK_sueldo FOREIGN KEY (id_sueldo) REFERENCES Sueldo(id_sueldo),
+    CONSTRAINT FK_empresa FOREIGN KEY (id_empresa) REFERENCES Empresa(id_empresa)
 
+)
 
-  grant select, insert, update, delete on Jornada to public
-  go
+grant select, insert, update, delete on Operario to public
 
-  -------------------------------------------------------------------------------
-  -- TABLE: Operacion
-  -------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- TABLE: Sueldo
+-------------------------------------------------------------------------------
+drop table if exists Sueldo
+create table Sueldo(
+    id_sueldo int not null,
+    sueldoTotal money not null,
+    sueldoBase money not null,
+    sueldoHorasExtra money not null,
+    PRIMARY KEY (id_sueldo)
+)
 
-  drop table if exists Operacion
-  go
-
-
-  create table Operacion(
-      id_operacion int,
-      id_jornada int,
-      tareaHora int,
-      totalProducido INT
-
-
-
-  )
-  go
-
-
-  grant select, insert, update, delete on Operacion to public
-  go
-
-  -------------------------------------------------------------------------------
-  -- TABLE: Hora
-  -------------------------------------------------------------------------------
-
-  drop table if exists Hora
-  go
-
-
-  create table Hora(
-      id_hora INT,
-      id_jornada int,
-      esExtra BOOLEAN,
-      horaRevicion TIME,
-  )
-  go
-
-
-  grant select, insert, update, delete on Hora to public
-  go
-
-  -------------------------------------------------------------------------------
-  -- TABLE: OperacionOperario
-  -------------------------------------------------------------------------------
-
-  drop table if exists OperacionOperario
-  go
-
-
-  create table OperacionOperario(
-      id_operacionOperario int,
-      id_operacion INT,
-      id_operario int,
-      id_hora int,
-      tareaHora int
-  )
-  go
-
-
-  grant select, insert, update, delete on OperacionOperario to public
-  go
-
-  -------------------------------------------------------------------------------
-  -- TABLE: Operario
-  -------------------------------------------------------------------------------
-
-  drop table if exists Operario
-  go
-
-
-  create table Operario(
-      id_operario int,
-      id_sueldo int
-  )
-  go
-
-
-  grant select, insert, update, delete on Operario to public
-  go
-
-  -------------------------------------------------------------------------------
-  -- TABLE: Sueldo
-  -------------------------------------------------------------------------------
-
-  drop table if exists Sueldo
-  go
-
-
-  create table Sueldo(
-      id_sueldo int
-      inicioSemana date,
-      finSemana date,
-      sueldoTotal money,
-      sueldaBase money,
-      sueldoHorasExtra money
-  )
-  go
-
-
-  grant select, insert, update, delete on Sueldo to public
-  go
+grant select, insert, update, delete on Sueldo to public
