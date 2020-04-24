1bd4a32e018308b284b8d68ec2422a2af66c8e91
Enoch454 <aegc.45@gmail.com>
2020-04-24 00:53:53 -0500

    sintaxis de script mql comprobada

--- mql.sql
+++ mql.sql
@@ -1,69 +1,62 @@
 CREATE DATABASE mql;
 use mql;
 
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Estilo
--------------------------------------------------------------------------------
-drop table if exists Estilo
+-- -----------------------------------------------------------------------------
+drop table if exists Estilo;
 create table Estilo(
     idEstilo int not null,
-    nombre VARCHAR(50)
-    tipo VARCHAR(20)
-    descripcion VARCHAR(200)
+    nombre VARCHAR(50),
+    tipo VARCHAR(20),
+    descripcion VARCHAR(200),
     imagenAdjunta BLOB,
     PRIMARY KEY (idEstilo)
-)
-grant select, insert, update, delete on Estilo to public
+);
+-- grant select, insert, update, delete on Estilo to public;
 
-
-
-
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Usuario
--------------------------------------------------------------------------------
-drop table if exists Usuario
+-- -----------------------------------------------------------------------------
+drop table if exists Usuario;
 create table Usuario(
     idUsuario int not null,
     nickName VARCHAR(20),
     contraseña VARCHAR(20),
-    PRIMARY KEY (idUsuario),
-)
-grant select, insert, update, delete on Usuario to public
+    PRIMARY KEY (idUsuario)
+);
+-- grant select, insert, update, delete on Usuario to public;
 
-
--------------------------------------------------------------------------------
--- TABLE: Admin
--------------------------------------------------------------------------------
-drop table if exists Encargado
-create table Encargado(
-    idAdmin int not null,
+-- -----------------------------------------------------------------------------
+-- TABLE: Admon
+-- -----------------------------------------------------------------------------
+drop table if exists Admon;
+create table Admon(
+    idAdmon int not null,
     nombre VARCHAR(50),
     idUsuario int not null,
-    PRIMARY KEY (idAdmin),
-    CONSTRAINT fk_admin_usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
-)
-grant select, insert, update, delete on Encargado to public
+    PRIMARY KEY (idAdmon),
+    CONSTRAINT fk_admon_usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+);
+-- grant select, insert, update, delete on Admon to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Empresa
--------------------------------------------------------------------------------
-drop table if exists Empresa
+-- -----------------------------------------------------------------------------
+drop table if exists Empresa;
 create table Empresa(
     idEmpresa int not null,
     nombre VARCHAR(50),
     telefono VARCHAR(10),
     direccion VARCHAR(50),
     PRIMARY KEY (idEmpresa)
-)
-grant select, insert, update, delete on Encargado to public
+);
+-- grant select, insert, update, delete on Empresa to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Secretario
--------------------------------------------------------------------------------
-drop table if exists Secretario
+-- -----------------------------------------------------------------------------
+drop table if exists Secretario;
 create table Secretario(
     idSecretario int not null,
     nombre VARCHAR(50),
@@ -74,14 +67,13 @@ create table Secretario(
     PRIMARY KEY (idSecretario),
     CONSTRAINT fk_secretario_usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
     CONSTRAINT fk_secretario_empresa FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa)
-)
-grant select, insert, update, delete on Secretario to public
+);
+-- grant select, insert, update, delete on Secretario to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Cliente
--------------------------------------------------------------------------------
-drop table if exists Cliente
+-- -----------------------------------------------------------------------------
+drop table if exists Cliente;
 create table Cliente(
     idCliente int not null,
     nombre VARCHAR(50),
@@ -90,32 +82,30 @@ create table Cliente(
     idEmpresa int not null,
     PRIMARY KEY (idCliente),
     CONSTRAINT fk_cliente_empresa FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa)
-)
-grant select, insert, update, delete on Cliente to public
+);
+-- grant select, insert, update, delete on Cliente to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Encargado
--------------------------------------------------------------------------------
-drop table if exists Encargado
+-- -----------------------------------------------------------------------------
+drop table if exists Encargado;
 create table Encargado(
     idEncargado int not null,
     nombre VARCHAR(50),
     direccion VARCHAR(50),
-    telefono VARCHAR(10)
+    telefono VARCHAR(10),
     idUsuario int not null,
     idEmpresa int not null,
     PRIMARY KEY (idEncargado),
     CONSTRAINT fk_encargado_usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
     CONSTRAINT fk_encargado_empresa FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa)
-)
-grant select, insert, update, delete on Encargado to public
+);
+-- grant select, insert, update, delete on Encargado to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Corte
--------------------------------------------------------------------------------
-drop table if exists Corte
+-- -----------------------------------------------------------------------------
+drop table if exists Corte;
 create table Corte(
     idCorte int not null,
     fechaPedido date not null,
@@ -127,16 +117,15 @@ create table Corte(
     idEncargado int not null,
     PRIMARY KEY (idCorte),
     CONSTRAINT fk_corte_estilo FOREIGN KEY (idEstilo) REFERENCES Estilo(idEstilo),
-    CONSTRAINT fk_corte_cliente FOREIGN KEY (idCliente) REFERENCES Estilo(idCliente),
-    CONSTRAINT fk_corte_encargado FOREIGN KEY (idEncargado) REFERENCES Estilo(idEncargado)
-)
-grant select, insert, update, delete on Corte to public
+    CONSTRAINT fk_corte_cliente FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente),
+    CONSTRAINT fk_corte_encargado FOREIGN KEY (idEncargado) REFERENCES Encargado(idEncargado)
+);
+-- grant select, insert, update, delete on Corte to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Operacion
--------------------------------------------------------------------------------
-drop table if exists Operacion
+-- -----------------------------------------------------------------------------
+drop table if exists Operacion;
 create table Operacion(
       idOperacion int not null,
       nombre int not null,
@@ -145,80 +134,77 @@ create table Operacion(
       idEstilo int not null,
       PRIMARY KEY (idOperacion),
       CONSTRAINT fk_operacion_estilo FOREIGN KEY (idEstilo) REFERENCES Estilo(idEstilo)
-)
-grant select, insert, update, delete on Operacion to public
+);
+-- grant select, insert, update, delete on Operacion to public
 
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Bono_Respaldo
--------------------------------------------------------------------------------
-drop table if exists Bono_Respaldo
+-- -----------------------------------------------------------------------------
+drop table if exists Bono_Respaldo;
 create table Bono_Respaldo(
-    idBono int not null,
+    idBonoRespaldo int not null,
     numPiezas int,
     porcentajeDelEstimado decimal,
     monto decimal,
     idOperacion int not null,
-    PRIMARY KEY (idBono),
+    PRIMARY KEY (idBonoRespaldo),
     CONSTRAINT fk_bonorespaldo_operacion FOREIGN KEY (idOperacion) REFERENCES Operacion(idOperacion)
-)
-grant select, insert, update, delete on Bono_Respaldo to public
+);
+-- grant select, insert, update, delete on Bono_Respaldo to public
 
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Hora
--------------------------------------------------------------------------------
-drop table if exists Hora
+-- -----------------------------------------------------------------------------
+drop table if exists Hora;
 create table Hora(
     idHora int not null,
     horaInicio time,
-    horaFin time
+    horaFin time,
     diaSemana VARCHAR(8),
     enUso bit,
     esExtra bit,
     idEmpresa int not null,
     PRIMARY KEY (idHora),
     CONSTRAINT fk_hora_empresa FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa)
-)
-grant select, insert, update, delete on Hora to public
+);
+-- grant select, insert, update, delete on Hora to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Corte_Operacion
--------------------------------------------------------------------------------
-drop table if exists Corte_Operacion
+-- -----------------------------------------------------------------------------
+drop table if exists Corte_Operacion;
 create table Corte_Operacion(
     idCorteOperacion int not null,
     numPiezasProcesadas int,
-    minutajeEstimado int
+    minutajeEstimado int,
     corteOperacionPredecedora int,
     idCorte int not null,
-    idOperacion int not null
+    idOperacion int not null,
     PRIMARY KEY (idCorteOperacion),
     CONSTRAINT fk_corteoperacion_corte FOREIGN KEY (idCorte) REFERENCES Corte(idCorte),
     CONSTRAINT fk_corteoperacion_operacion FOREIGN KEY (idOperacion) REFERENCES Operacion(idOperacion)
-)
-grant select, insert, update, delete on Corte_Operacion to public
+);
+-- grant select, insert, update, delete on Corte_Operacion to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Bono
--------------------------------------------------------------------------------
-drop table if exists Bono
+-- -----------------------------------------------------------------------------
+drop table if exists Bono;
 create table Bono(
     idBono int not null,
-    numPiezas int
-    porcentajeDelEstimado decimal
-    monto decimal
+    numPiezas int,
+    porcentajeDelEstimado decimal,
+    monto decimal,
     idCorteOperacion int not null,
     PRIMARY KEY (idBono),
     CONSTRAINT fk_bono_corteoperacion FOREIGN KEY (idCorteOperacion) REFERENCES Corte_Operacion(idCorteOperacion)
-)
-grant select, insert, update, delete on Bono to public
+);
+-- grant select, insert, update, delete on Bono to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Corte_Operacion_Hora
--------------------------------------------------------------------------------
-drop table if exists Corte_Operacion_Hora
+-- -----------------------------------------------------------------------------
+drop table if exists Corte_Operacion_Hora;
 create table Corte_Operacion_Hora(
     idCorteOperacionHora int not null,
     fecha date,
@@ -227,15 +213,13 @@ create table Corte_Operacion_Hora(
     PRIMARY KEY (idCorteOperacionHora),
     CONSTRAINT fk_coh_corteoperacion FOREIGN KEY (idCorteOperacion) REFERENCES Corte_Operacion(idCorteOperacion),
     CONSTRAINT fk_coh_hora FOREIGN KEY (idHora) REFERENCES Hora(idHora)
-)
-grant select, insert, update, delete on Corte_Operacion_Hora to public
+);
+-- grant select, insert, update, delete on Corte_Operacion_Hora to public
 
-
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Operario
--------------------------------------------------------------------------------
-drop table if exists Operario
+-- -----------------------------------------------------------------------------
+drop table if exists Operario;
 create table Operario(
     idOperario int not null,
     nombre varchar(50),
@@ -247,18 +231,16 @@ create table Operario(
     estaOcupado bit,
     idEmpresa int not null,
     idCorteOperacion int,
-    PRIMARY KEY (id_operario),
+    PRIMARY KEY (idOperario),
     CONSTRAINT FK_operario_corteoperacion FOREIGN KEY (idCorteOperacion) REFERENCES Corte_Operacion(idCorteOperacion),
     CONSTRAINT FK_operario_empresa FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa)
-)
+);
+-- grant select, insert, update, delete on Operario to public
 
-grant select, insert, update, delete on Operario to public
-
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Corte_Operacion_Hora_Operario
--------------------------------------------------------------------------------
-drop table if exists Corte_Operacion_Hora_Operario
+-- -----------------------------------------------------------------------------
+drop table if exists Corte_Operacion_Hora_Operario;
 create table Corte_Operacion_Hora_Operario(
     idCorteOperacionHoraOperario int not null,
     numOperacionesRealizadas int,
@@ -267,14 +249,13 @@ create table Corte_Operacion_Hora_Operario(
     PRIMARY KEY (idCorteOperacionHoraOperario),
     CONSTRAINT fk_coho_coh FOREIGN KEY (idCorteOperacionHora) REFERENCES Corte_Operacion_Hora(idCorteOperacionHora),
     CONSTRAINT fk_coho_operario FOREIGN KEY (idOperario) REFERENCES Operario(idOperario)
-)
-grant select, insert, update, delete on Corte_Operacion_Hora to public
+);
+-- grant select, insert, update, delete on Corte_Operacion_Hora_Operario to public
 
-
--------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- TABLE: Sueldo
--------------------------------------------------------------------------------
-drop table if exists Sueldo
+-- -----------------------------------------------------------------------------
+drop table if exists Sueldo;
 create table Sueldo(
     idSueldo int not null,
     fechaCorte date,
@@ -287,8 +268,6 @@ create table Sueldo(
     comentarioOtrosBonos varchar(100),
     idOperario int not null,
     PRIMARY KEY (idSueldo),
-    CONSTRAINT FK_operario_corteoperacion FOREIGN KEY (idCorteOperacion) REFERENCES Corte_Operacion(idCorteOperacion)
-
-)
-
-grant select, insert, update, delete on Sueldo to public
+    CONSTRAINT FK_sueldo_operario FOREIGN KEY (idOperario) REFERENCES Operario(idOperario)
+);
+-- grant select, insert, update, delete on Sueldo to public
